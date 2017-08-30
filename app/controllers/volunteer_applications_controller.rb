8c83d040b424d06f4534b0e76c08e51f50b91e31
Dan Engel <dengel29@gmail.com>
2017-08-30 14:01:33 +0800

    accept and decline changed and fixed

--- app/controllers/volunteer_applications_controller.rb
+++ app/controllers/volunteer_applications_controller.rb
@@ -2,14 +2,15 @@ class VolunteerApplicationsController < ApplicationController
 
   def confirm
     @event = current_user.events.find(params[:event_id])
-    @volunteer_application = @event.volunteer_applications.find(params[:id])
+    @volunteer_application = @event.volunteer_applications.find(params[:volunteer_application_id])
     @volunteer_application.accept!
 
     redirect_to event_path(@volunteer_application.event_id)
   end
 
   def decline
-    @volunteer_application = VolunteerApplication.find_by(params[:id])
+    @event = current_user.events.find(params[:event_id])
+    @volunteer_application = @event.volunteer_applications.find(params[:volunteer_application_id])
     @volunteer_application.decline!
 
     redirect_to event_path(@volunteer_application.event_id)
